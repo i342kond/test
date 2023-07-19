9499847b84bf0ca20d1ca7426316c50beefb1778
i342kond <138637319+i342kond@users.noreply.github.com>
2023-07-19 10:47:58 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,6 +2,6 @@
 <?php
   foreach(file('/usr/share/dict/words') as $line){
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-if(strpos($line, "abc")>=0)echo $line;
+if(strpos($line, 'xy') !== false)echo $line;
   }
 ?>
